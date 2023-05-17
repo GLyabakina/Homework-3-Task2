8f6d766ffc7d080989e03d5f9912f4d92906eed0
Galina <razumova-g@mail.ru>
2023-05-17 22:20:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weight = 98;
+        double height = 1.87;
+        int bmi = service.calculate(98, 1.87);
+
+        System.out.println(bmi);
+    }
+}
